a36019808d37a7e56b35a301f965ba103dfffe2d
simpllex <44024935+simpllex@users.noreply.github.com>
2020-03-03 11:03:20 +0400

    Add files via upload
    страница загрузки текста/файлов, обработка файлов, и пример результата работы.

--- action.php
+++ action.php
@@ -1,31 +1,47 @@
-Введен текст, <?php 
-$myString = $_POST['name'];
-$myString = mb_strtolower($myString);
-//заменить знаки препинания на пустые строки
-$myString = str_replace([',', '.', ';', '!', '?', ' - '], '', $myString);
-$pieces = explode(" ", $myString);
-$array = [];
+<pre>
+<?php 
+//подсчитывает количетсво использований каждого слова
+function analysis($myString){
+    $myString = mb_strtolower($myString);
+    //заменить знаки препинания на пустые строки
+    $myString = str_replace([',', '.', ';', ':', '!', '?', ' -', '=', '(', ')'], '', $myString);
+    $myString = str_replace('  ', ' ', $myString);
+    $pieces = explode(" ", $myString);
+    $array = [];
 
-foreach ($pieces as $key)
-if (array_key_exists ($key, $array)){
-    $array[$key]++;
-} else {
-    $array[$key] = 1;
-} 
-
-$file = 'file.csv';
-// touch($file);
-// $current = "";
-// foreach ($array as $key => $value) {
-//     $current .= $key . ' -- ' . $value . "\n";
-// }
-// file_put_contents($file, $current);
-
-$fp = fopen($file, 'w');
-
-foreach ($array as $key => $value) {
-    fputcsv($fp, array($key, $value));
+    foreach ($pieces as $key)
+    if (array_key_exists ($key, $array)){
+        $array[$key]++;
+    } else {
+        $array[$key] = 1;
+    } 
+    return $array;
+}
+//сохраняет результат в файл с уникальным именем
+function to_csv($myArray, $str){
+    $file = $str . ((string) date("Y-m-d H:i:s")) . '.csv';
+    $fp = fopen($file, 'a+');
+    foreach ($myArray as $key => $value) {
+        fputcsv($fp, array($key, $value));
+    }
+    fclose($fp);
 }
 
-fclose($fp);
+
+if (!empty($_POST['test'])) {
+    $myString = $_POST['test'];
+    to_csv(analysis($myString), 'txt');
+    echo ("Успешно обработана строка\n");
+} else {
+    echo ("Пустая строка\n");
+}
+
+
+if ($_FILES['file']['name'] != "") {
+    $myFileString = file_get_contents($_FILES['file']['tmp_name']);
+    to_csv(analysis($myFileString), 'file');
+    echo ("Успешно обработан файл\n");
+} else {
+    echo ("Нет файла\n");
+}
 ?>
--- index.php
+++ index.php
@@ -1,4 +1,5 @@
-<form action="action.php" method="post">
- <p>Ваш текст: <input type="textarea" name="name" /></p>
+<form action="action.php" method="post" enctype=multipart/form-data>
+ <p>Ваш текст: <input type="textarea" name="test" /></p>
+  <p>Ваш файл:  <input type = "file" name = "file" /></p>
  <p><input type="submit" /></p>
 </form>
